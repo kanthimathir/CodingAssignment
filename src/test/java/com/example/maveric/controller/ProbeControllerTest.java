e5bf6edb8e87dfed79ff3a716f54d17e4f75e971
kramaswamy <kramaswamy@virtusa.com>
2025-04-18 19:35:15 +0100

    Adding the controller test class

---
+++ src/test/java/com/example/maveric/controller/ProbeControllerTest.java
@@ -0,0 +1,14 @@
+package com.example.maveric.controller;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+import org.junit.jupiter.api.Test;
+
+class ProbeControllerTest {
+
+	@Test
+	void test() {
+		fail("Not yet implemented");
+	}
+
+}

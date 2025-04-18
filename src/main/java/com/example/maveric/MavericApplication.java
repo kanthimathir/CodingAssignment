a1775f4410243eeddda61ced26e04ff8afa7f4b8
kramaswamy <kramaswamy@virtusa.com>
2025-04-18 08:28:22 +0100

    initial version

---
+++ src/main/java/com/example/maveric/MavericApplication.java
@@ -0,0 +1,13 @@
+package com.example.maveric;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MavericApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MavericApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/maveric/MavericApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.maveric;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MavericApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

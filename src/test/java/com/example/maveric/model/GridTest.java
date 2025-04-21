ed617d17d738eea8bae364f6f485398c02a39ffc
kramaswamy <kramaswamy@virtusa.com>
2025-04-21 01:55:14 +0100

    Adding more Positive test cases

--- src/test/java/com/example/maveric/model/GridTest.java
+++ src/test/java/com/example/maveric/model/GridTest.java
@@ -3,17 +3,32 @@ package com.example.maveric.model;
 import static org.junit.jupiter.api.Assertions.*;
 
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvSource;
 
 class GridTest {
 
 	int width = 10;
 	int height = 10;
+	Grid grid = new Grid(width, height);
 	
 	@Test
-	void getGridTest() {
-		Grid grid = new Grid(width, height);
+	void getGridTest() {		
 		assertEquals(10, grid.getWidth());
 		assertEquals(10, grid.getHeight());
 	}
-
+	
+	@ParameterizedTest(name = "{index} => x={0}, y={1}")
+	@CsvSource(value = {"1, 2"}, ignoreLeadingAndTrailingWhitespace = true)
+	void checkBoundsPositive(int x, int y) {
+		boolean withinBounds = grid.isWithinBounds(x, y);
+		assertTrue(withinBounds);
+	}
+	
+	@ParameterizedTest(name = "{index} => x={0}, y={1}")
+	@CsvSource(value = {"11, 2"}, ignoreLeadingAndTrailingWhitespace = true)
+	void checkBoundsNegative(int x, int y) {
+		boolean withinBounds = grid.isWithinBounds(x, y);
+		assertFalse(withinBounds);
+	}
 }
--- src/test/java/com/example/maveric/service/ProbeServiceTest.java
+++ src/test/java/com/example/maveric/service/ProbeServiceTest.java
@@ -12,7 +12,7 @@ class ProbeServiceTest {
 	private ProbeService probeService = new ProbeService();
 	
 	@Test
-    void shouldMoveForward() {
+    void shouldMoveForwardFromEast() {
         Position start = new Position(2, 2, "EAST");
         Command command = Command.MOVE_FORWARD;
         
@@ -24,7 +24,7 @@ class ProbeServiceTest {
     }
 	
 	@Test
-    void shouldMoveBackward() {
+    void shouldMoveBackwardFromEast() {
         Position start = new Position(2, 2, "EAST");
         Command command = Command.MOVE_BACKWARD;
         
@@ -36,7 +36,7 @@ class ProbeServiceTest {
     }
 	
 	@Test
-	void shouldTurnRight() {
+	void shouldTurnRightFromEast() {
 	    Position start = new Position(0, 0, "EAST");
 	    Command command = Command.TURN_RIGHT;
 
@@ -46,12 +46,144 @@ class ProbeServiceTest {
 	}
 	
 	@Test
-	void shouldTurnLeft() {
+	void shouldTurnLeftFromEast() {
 	    Position start = new Position(0, 0, "EAST");
 	    Command command = Command.TURN_LEFT;
 
 	    Position end = probeService.executeCommands(start, command);
 
 	    assertEquals("NORTH", end.getDirection());
+	}
+	
+	@Test
+    void shouldMoveForwardFromWest() {
+        Position start = new Position(2, 2, "WEST");
+        Command command = Command.MOVE_FORWARD;
+        
+        Position end = probeService.executeCommands(start, command);
+
+        assertEquals(1, end.getX());
+        assertEquals(2, end.getY());
+        assertEquals("WEST", end.getDirection());
+    }
+	
+	@Test
+    void shouldMoveBackwardFromWest() {
+        Position start = new Position(2, 2, "WEST");
+        Command command = Command.MOVE_BACKWARD;
+        
+        Position end = probeService.executeCommands(start, command);
+
+        assertEquals(3, end.getX());
+        assertEquals(2, end.getY());
+        assertEquals("WEST", end.getDirection());
+    }
+	
+	@Test
+	void shouldTurnRightFromWest() {
+	    Position start = new Position(0, 0, "WEST");
+	    Command command = Command.TURN_RIGHT;
+
+	    Position end = probeService.executeCommands(start, command);
+
+	    assertEquals("NORTH", end.getDirection());
+	}
+	
+	@Test
+	void shouldTurnLeftFromWest() {
+	    Position start = new Position(0, 0, "WEST");
+	    Command command = Command.TURN_LEFT;
+
+	    Position end = probeService.executeCommands(start, command);
+
+	    assertEquals("SOUTH", end.getDirection());
+	}	
+	
+	@Test
+    void shouldMoveForwardFromSounth() {
+        Position start = new Position(2, 2, "SOUTH");
+        Command command = Command.MOVE_FORWARD;
+        
+        Position end = probeService.executeCommands(start, command);
+
+        assertEquals(2, end.getX());
+        assertEquals(1, end.getY());
+        assertEquals("SOUTH", end.getDirection());
+    }
+	
+	@Test
+    void shouldMoveBackwardFromSounth() {
+        Position start = new Position(2, 2, "SOUTH");
+        Command command = Command.MOVE_BACKWARD;
+        
+        Position end = probeService.executeCommands(start, command);
+
+        assertEquals(2, end.getX());
+        assertEquals(3, end.getY());
+        assertEquals("SOUTH", end.getDirection());
+    }
+	
+	@Test
+	void shouldTurnRightFromSounth() {
+	    Position start = new Position(0, 0, "SOUTH");
+	    Command command = Command.TURN_RIGHT;
+
+	    Position end = probeService.executeCommands(start, command);
+
+	    assertEquals("WEST", end.getDirection());
+	}
+	
+	@Test
+	void shouldTurnLeftFromSounth() {
+	    Position start = new Position(0, 0, "SOUTH");
+	    Command command = Command.TURN_LEFT;
+
+	    Position end = probeService.executeCommands(start, command);
+
+	    assertEquals("EAST", end.getDirection());
+	}
+	
+	@Test
+    void shouldMoveForwardFromNorth() {
+        Position start = new Position(2, 2, "NORTH");
+        Command command = Command.MOVE_FORWARD;
+        
+        Position end = probeService.executeCommands(start, command);
+
+        assertEquals(2, end.getX());
+        assertEquals(3, end.getY());
+        assertEquals("NORTH", end.getDirection());
+    }
+	
+	@Test
+    void shouldMoveBackwardFromNorth() {
+        Position start = new Position(2, 2, "NORTH");
+        Command command = Command.MOVE_BACKWARD;
+        
+        Position end = probeService.executeCommands(start, command);
+
+        assertEquals(2, end.getX());
+        assertEquals(1, end.getY());
+        assertEquals("NORTH", end.getDirection());
+    }
+	
+	@Test
+	void shouldTurnRightFromNorth() {
+	    Position start = new Position(0, 0, "NORTH");
+	    Command command = Command.TURN_RIGHT;
+
+	    Position end = probeService.executeCommands(start, command);
+
+	    assertEquals("EAST", end.getDirection());
+	}
+	
+	@Test
+	void shouldTurnLeftFromNorth() {
+	    Position start = new Position(0, 0, "NORTH");
+	    Command command = Command.TURN_LEFT;
+
+	    Position end = probeService.executeCommands(start, command);
+
+	    assertEquals("WEST", end.getDirection());
 	}
 }
